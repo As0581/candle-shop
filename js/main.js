11b353e929198ef42d846bb2bf3ffaa5d7b5d0d7
As0581 <zagirovaslan1@gmail.com>
2025-08-26 19:36:43 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const menuBtn = document.querySelector('.menu__btn')
+const menuMobile = document.querySelector('.header__menu-list')
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open')
+})
+
+const swiperOne = new Swiper('.feedback__slider', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener('click', () => {
+
+    const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text')
+
+    accordeonTitle.classList.toggle('accordeon__title--active')
+    currentText.classList.toggle('accordeon__text--visible')
+
+    if(currentText.classList.contains('accordeon__text--visible')){
+      currentText.style.maxHeight = 213 + 'px'
+    }else{
+      currentText.style.maxHeight = null
+    }
+
+
+  })
+});
+
